d52db9d7943ee2c573faf851d443e3d2cbfb4515
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-11-27 12:19:14 -0300

    context created, addtocart and removetocart too

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import AppContext from '../context/AppContext'
+import useInitialState from '../hooks/useInitialState'
 import Layout from '../components/Layout'
 import Home from '../containers/Home'
 import Checkout from '../containers/Checkout'
@@ -10,19 +11,23 @@ import Success from '../containers/Success'
 import NotFound from '../containers/NotFound'
 
 const App = () => {
+  const initialState = useInitialState()
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
 
