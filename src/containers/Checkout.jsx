05deed580fd1085727eec81ca812c7f7a27ddc62
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-11-30 20:16:45 -0300

    added react helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import AppContext from '../context/AppContext'
 import '../styles/components/Checkout.css'
 
@@ -19,29 +20,34 @@ const Checkout = () => {
   }
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? <h3>Orders list</h3> : <h3>Without orders</h3>}
-        {cart.map((item) => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>$ {item.price}</span>
+    <div>
+      <Helmet>
+        <title>Order list - Platzi Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? <h3>Orders list</h3> : <h3>Without orders</h3>}
+          {cart.map((item) => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>$ {item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                <i className="fas fa-trash-alt" />
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Total price: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continue</button>
-          </Link>
+          ))}
         </div>
-      )}
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Total price: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continue</button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
