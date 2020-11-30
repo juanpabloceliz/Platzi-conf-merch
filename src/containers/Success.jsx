3154facd5704af843eada892eb6bed50ebb55bdb
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-11-30 11:34:36 -0300

    google maps ready

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import AppContext from '../context/AppContext'
 import Map from '../components/Map'
+import useGoogleAddress from '../hooks/useGoogleAddress'
 import '../styles/components/Success.css'
 
 const Success = () => {
   const { state } = useContext(AppContext)
   const { buyer } = state
+  const location = useGoogleAddress(buyer[0].address)
 
   return (
     <div className="Succes">
@@ -13,7 +15,7 @@ const Success = () => {
         <h2>{`${buyer.name} Thanks for buy`}</h2>
         <span>Your order will be at door soon...</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
